7bc3415e08d0059fc2c65d9b12a739f7c09ca246
William Davis <greg.davis55@gmail.com>
2021-01-02 18:53:56 -0600

    UFO Files

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+
+console.log(addition(4, 5)); 
